f14d83b5dda538dba9db43a6276ead8ff8f35314
Arturo Escudero <darkturo@gmail.com>
2025-02-16 22:51:21 +0100

    Update benchmarker to launch more types of benchmarks
    Among them the idea is to embed benchmarks with pyperf for python
    libraries, but also pave the path for custom benchmark binaries build
    specifically to test a given library

--- benchmarks/benchmark_all_tools.sh
+++ benchmarks/benchmark_all_tools.sh
@@ -3,7 +3,7 @@
 # Run the bencharker for all the configured tools in this directory.
 find -type f -name 'benchmark.cfg' | while read -r file; do
 	echo "Running benchmarker for $(basename $(dirname $file)) ($file)"
-	./benchmarker -c "$file"
+	../scripts/benchmarker -c "$file"
 	echo "-----------------------------------"
 	echo ""
 done
