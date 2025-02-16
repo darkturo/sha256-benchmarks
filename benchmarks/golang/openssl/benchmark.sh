1fde411e3919d2e9c3a00c7ba9f8a3786eb83dba
Arturo Escudero <darkturo@gmail.com>
2025-02-16 10:33:52 +0100

    Add results of the cmd line openssl bench, avg run 2.7 ms

--- benchmarks/golang/openssl/benchmark.sh
+++ benchmarks/golang/openssl/benchmark.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 WARMUP=5
-hyperfine -N --warmup $WARMUP 'openssl dgst -sha256 input.txt' --export-json openssl-bechmark.json
+hyperfine -N --warmup $WARMUP 'openssl dgst -sha256 input.txt' --export-json result-benchmark-openssl.json
