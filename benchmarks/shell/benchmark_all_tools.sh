e3af66a03f423035ab2d0212cb596d47938eaa5b
Arturo Escudero <darkturo@gmail.com>
2025-02-16 14:34:30 +0100

    Add a tool to run all the shell benchmarks at once
    Also update all the results

---
+++ benchmarks/shell/benchmark_all_tools.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Run the bencharker for all the configured tools in this directory.
+find -type f -name 'benchmark.cfg' | while read -r file; do
+	echo "Running benchmarker for $(basename $(dirname $file)) ($file)"
+	./benchmarker -c "$file"
+	echo "-----------------------------------"
+	echo ""
+done
