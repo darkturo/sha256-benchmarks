214df58d229c1903fd0df590a1dd8ec5762e6cd7
Arturo Escudero <darkturo@gmail.com>
2025-02-28 01:42:18 +0100

    Extend build_benchmark to support a list of libs, update openssl

--- benchmarks/cmake/Benchmark.cmake
+++ benchmarks/cmake/Benchmark.cmake
@@ -1,6 +1,7 @@
 cmake_minimum_required(VERSION 3.10)
 
-function(build_benchmark Name Library)
+# build_benchmark(Name [LIBRARIES ...])
+function(build_benchmark Name)
     set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
     # common stuff
@@ -15,13 +16,27 @@ function(build_benchmark Name Library)
         add_compile_definitions(${Name} ENABLE_STDOUT_DEBUG=1)
     endif()
 
+    # Weird contraption I did to look similar to other cmake functions
+    set(LIBS)
+    set(COLLECT_LIBS OFF)
+    message(STATUS "LIBS: ${ARGN}")
+    foreach(arg IN LISTS ARGN)
+        if (COLLECT_LIBS)
+            list(APPEND LIBS ${arg})
+        elseif (${arg} STREQUAL "LIBRARIES")
+            set(COLLECT_LIBS ON)
+        endif()
+    endforeach()
+    message(STATUS "LIBS: ${LIBS}")
+
     target_link_libraries(${Name}
             PRIVATE
             Folly::folly Folly::follybenchmark
             fmt::fmt
             Boost::filesystem Boost::system
             gflags
-            ${Library}
-            )
+            ${LIBS}
+    )
+
 endfunction()
 
