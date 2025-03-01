4e98a140da71fefcc97c694c8fce28401d66e30b
Arturo Escudero <darkturo@gmail.com>
2025-03-01 02:27:29 +0100

    Fix draft and make the idea to work
    Is 3ns slower tho ...

--- benchmarks/benchmark.h
+++ benchmarks/benchmark.h
@@ -7,10 +7,55 @@
 #include <folly/Benchmark.h>
 #include <gflags/gflags.h>
 
+
+DEFINE_string(input, "", "File containing the string to be hashed");
+
 namespace benchmark {
     std::string message;
 
-    DEFINE_string(input, "", "File containing the string to be hashed");
+    template <typename T>
+    concept ByteType = requires(T a) {
+        { a } -> std::convertible_to<char>;
+    };
+
+    template <ByteType T, int DigestSize>
+    class digest_trait {
+    public:
+        typedef void hash_fun_type(const std::string &message, T *digest);
+
+        digest_trait(hash_fun_type *fun) : hash_fun(fun) {}
+
+        void hash(const std::string &message, T *digest) {
+            hash_fun(message, digest);
+        }
+
+        void print(T *digest) {
+            std::cout << "SHA-256 hash of \"" << message << "\": ";
+            for (int i = 0; i < DigestSize; ++i) {
+                std::cout << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(digest[i]);
+            }
+            std::cout << std::endl;
+        }
+    private:
+        hash_fun_type *hash_fun;
+    };
+
+    template <ByteType T,
+              int DigestSize,
+              typename trait = digest_trait<T, DigestSize>>
+    void bechmarker(void fun(const std::string &message, T *digest), uint32_t n) {
+        T digest[DigestSize];
+        auto hasher = trait(fun);
+        for (int i = 0; i < n; ++i) {
+            hasher.hash(message, digest);
+#ifdef ENABLE_STDOUT_DEBUG
+            hasher.print(digest);
+#else
+            folly::doNotOptimizeAway(digest);
+#endif
+        }
+    }
+
 
     int run() {
         if (FLAGS_input.empty()) {
@@ -27,31 +72,5 @@ namespace benchmark {
 
         folly::runBenchmarks();
         return 0;
-    }
-
-    concept ByteType = requires(T a) {
-        { a } -> std::convertible_to<char>;
-    };
-
-    template <typename digest_trait, typename print_digest_trait, class ByteType, int DigestSize>
-    void digest_benchmark(uint32_t n) {
-        ByteType digest[DigestSize];
-        for (int i = 0; i < n; ++i) {
-            digest_trait(message, digest);
-#ifdef ENABLE_STDOUT_DEBUG
-            print_digest_trait(digest);
-#else
-            folly::doNotOptimizeAway(digest);
-#endif
-            }
-    }
-
-    template <class ByteType, int DigestSize>
-    void print_digest_default(ByteType *digest) {
-        std::cout << "SHA-256 hash of \"" << message << "\": ";
-        for (int i = 0; i < DigestSize; ++i) {
-            std::cout << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(digest[i]);
-        }
-        std::cout << std::endl;
     }
 }
--- benchmarks/wolfssl/main.cc
+++ benchmarks/wolfssl/main.cc
@@ -7,51 +7,23 @@
 #include <wolfssl/options.h>
 #include <wolfssl/wolfcrypt/sha256.h>
 
+#include "../benchmark.h"
 
-DEFINE_string(input, "", "File containing the string to be hashed");
+template <benchmark::ByteType T>
+void wolfssl_sha256(const std::string &message, T *digest) {
+    wc_Sha256 sha256;
 
-std::string message;
+    wc_InitSha256(&sha256);
+    wc_Sha256Update(&sha256, reinterpret_cast<const byte *>(message.c_str()), message.size());
+    wc_Sha256Final(&sha256, digest);
+}
 
-BENCHMARK(wolfssl_sha256, n) {
-    byte hash[WC_SHA256_DIGEST_SIZE];
-
-
-    for (int i = 0; i < n; ++i) {
-        wc_Sha256 sha256;
-
-        wc_InitSha256(&sha256);
-        wc_Sha256Update(&sha256, reinterpret_cast<const byte*>(message.c_str()), message.size());
-        wc_Sha256Final(&sha256, hash);
-
-
-#ifdef ENABLE_STDOUT_DEBUG
-        std::cout << "SHA-256 hash of \"" << message << "\": ";
-        for (int i = 0; i < WC_SHA256_DIGEST_SIZE; ++i) {
-            std::cout << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(hash[i]);
-        }
-        std::cout << std::endl;
-#else
-        folly::doNotOptimizeAway(hash);
-#endif
-    }
+BENCHMARK(digest_bench, n) {
+    benchmark::bechmarker<byte, WC_SHA256_DIGEST_SIZE>(wolfssl_sha256, n);
 }
 
 int main(int argc, char **argv) {
     gflags::SetUsageMessage("WolfSSL SHA256 benchmark");
     gflags::ParseCommandLineFlags(&argc, &argv, true);
-
-    if (FLAGS_input.empty()) {
-	std::cerr << "Use the '-help' option to see how to run this" << std::endl;
-	return 1;
-    }
-
-    std::cout << "File to be hashed: " << FLAGS_input << std::endl;
-    std::ifstream file(FLAGS_input);
-    std::string input_str((std::istreambuf_iterator<char>(file)), 
-		           std::istreambuf_iterator<char>());
-    file.close();
-    message = input_str;
-
-    folly::runBenchmarks();
-    return 0;
+    return benchmark::run();
 }
